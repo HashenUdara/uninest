815e4d51567935d242fa916baad644e83531c59c
Keshani14 <keshaninuwanthika2@gmail.com>
2025-12-31 16:18:52 +0530

    Revert "errors fixed"
    This reverts commit e504148f960aff53998c22a22f6c0286cdb508d3.

--- src/main/java/com/uninest/controller/DemoServlet.java
+++ src/main/java/com/uninest/controller/DemoServlet.java
@@ -15,6 +15,7 @@ import java.util.Map;
  * No business logic - just routing to views.
  */
 @WebServlet(urlPatterns = {
+<<<<<<< HEAD
         "/student/community",
         "/student/community/new-post",
         "/student/community/post-details",
@@ -42,6 +43,7 @@ public class DemoServlet extends HttpServlet {
 
     private static final Map<String, String> ROUTE_MAP = new HashMap<>();
 
+=======
     "/student/community",
     "/student/community/new-post",
     "/student/community/post-details",
@@ -71,7 +73,8 @@ public class DemoServlet extends HttpServlet {
 public class DemoServlet extends HttpServlet {
     
     private static final Map<String, String> ROUTE_MAP = new HashMap<>();
-
+    
+>>>>>>> 14826574bbb572d2e8d81f6e278c85dbbf966f44
     static {
         // Community routes
         ROUTE_MAP.put("/student/community", "/WEB-INF/views/student/community/index.jsp");

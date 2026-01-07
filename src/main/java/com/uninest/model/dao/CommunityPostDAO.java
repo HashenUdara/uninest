accedc37d892e40de78e66194da5cb3b83564693
Viman Fernando <vimannimneth@gmail.com>
2026-01-07 15:07:19 +0530

    re created the delete function
    my posts feature is full functional

--- src/main/java/com/uninest/model/dao/CommunityPostDAO.java
+++ src/main/java/com/uninest/model/dao/CommunityPostDAO.java
@@ -164,4 +164,22 @@ public class CommunityPostDAO {
             throw new RuntimeException("Error updating community post", e);
         }
     }
+
+    /**
+     * Deletes a community post by its ID.
+     * Also deletes associated likes and comments due to CASCADE constraint.
+     * @param postId The ID of the post to delete
+     * @return true if deletion was successful, false otherwise
+     */
+    public boolean delete(int postId) {
+        String sql = "DELETE FROM community_posts WHERE id = ?";
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setInt(1, postId);
+            int rowsAffected = ps.executeUpdate();
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException("Error deleting community post", e);
+        }
+    }
 }
